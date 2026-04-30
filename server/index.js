bfdf754981776cc20c74f01cc7190dd6276c2912
Aditya Kauthkar <adityakauthkar2021.it@mmcoe.edu.in>
2026-04-30 21:27:41 +0530

    fix env

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000", 
+    origin: "https://study-notion-am5dqpchf-adityakauthkars-projects.vercel.app", 
     credentials: true,
   })
 );
